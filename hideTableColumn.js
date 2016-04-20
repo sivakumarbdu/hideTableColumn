8fbafb94d0270d32a4098552c58a98ce2bebc230
balakrishanan <balakrishanan.k@spritle.com>
2016-04-20 16:43:45 +0530

    initial js updates

--- hideTableColumn.js
+++ hideTableColumn.js
@@ -5,5 +5,33 @@
  * 
  *
  */
- 
- 
+
+$(document).ready(function() {
+	$("div#productColumnId ul li input").on("change", function(event) {
+		event.preventDefault();
+
+		var dataId = []
+		$("div#productColumnId ul li input:checked").each(function(index) {
+			name = $(this).attr("id");
+			dataId.push(name);
+		});
+		$("#ajax-box-inner").text(dataId);
+		/*var pathVal = "${pageContext.request.contextPath}";
+		$.ajax({
+			url : pathVal + '/productoPantalla.do?metodo=saveTableConfigList',
+			dataType : "json",
+			contentType : "application/json; charset=utf-8",
+			data : {
+				order : JSON.stringify(dataId),
+				tipo_objecto : 1
+			}
+		});*/
+
+		column_id = $(this).attr("id")
+		if ($(this).prop('checked')) {
+			$('.' + column_id).show();
+		} else {
+			$('.' + column_id).hide();
+		}
+	})
+});
