2c6c81bf2d4eedbbfe5b79ba434bb5ded40b20d5
balakrishanan <balakrishanan.k@spritle.com>
2016-05-04 12:53:38 +0530

    Js Alignment Fix

--- hideTableColumn.js
+++ hideTableColumn.js
@@ -5,55 +5,66 @@
  * 
  *
  */
+$(document).ready(
+		function() {
 
-$(document).ready(function() {
-	
-	/*
-	$.ajax({url: '/myAjax?method=getSaveedTableConfigList',
-	 	   dataType: "json",
-	        contentType: "application/json; charset=utf-8",
-	          data: { tipo_id: 1}}).done(function(data) {
-	        	  if(data.success == false)
-        		  {data["column"] = ["firstNameId","lastNameId","emailId","yearId"]} 
-	        var allCheckedColumnFilters = []
-		$( "div#productColumnId ul li input:checked" ).each(function( index ) {
-			 name = $(this).attr("id");
-			 allCheckedColumnFilters.push(name);
+			/*$.ajax({
+				url : '/myAjax?method=getSaveedTableConfigList',
+				dataType : "json",
+				contentType : "application/json; charset=utf-8",
+				data : {
+					tipo_id : 1
+				}
+			}).done(
+					function(data) {
+						if (data.success == false) {
+							data["column"] = [ "firstNameId", "lastNameId",
+									"emailId", "yearId" ]
+						}
+						var allCheckedColumnFilters = []
+						$("div#productColumnId ul li input:checked").each(
+								function(index) {
+									name = $(this).attr("id");
+									allCheckedColumnFilters.push(name);
+								});
+						uncheckedFilters = $(allCheckedColumnFilters).not(
+								data["column"]).get();
+
+						$.each(uncheckedFilters, function(index, value) {
+							$("#" + value + "").prop("checked", false)
+							$("." + value).hide();
+						});
+					}).fail(function() {
+			});*/
+
+			$("div#productColumnId ul li input").on(
+					"change",
+					function(event) {
+						event.preventDefault();
+
+						var dataId = []
+						$("div#productColumnId ul li input:checked").each(
+								function(index) {
+									name = $(this).attr("id");
+									dataId.push(name);
+								});
+						$("#ajax-box-inner").text(JSON.stringify(dataId));
+
+						/*$.ajax({
+							url : '/myAjax?method=saveTableConfigList',
+							dataType : "json",
+							contentType : "application/json; charset=utf-8",
+							data : {
+								order : JSON.stringify(dataId),
+								tipo_objecto : 1
+							}
+						});*/
+
+						column_id = $(this).attr("id")
+						if ($(this).prop('checked')) {
+							$('.' + column_id).show();
+						} else {
+							$('.' + column_id).hide();
+						}
+					})
 		});
-	        	  uncheckedFilters = $(allCheckedColumnFilters).not(data["column"]).get();
-	        	  
-	        	  $.each(uncheckedFilters,function( index, value ) {
-	        		  $("#"+value+"").prop("checked",false)
-	        		  $("."+value).hide();
-	     		 });
-	        	}).fail(function() {
-		        });*/
-	
-	$("div#productColumnId ul li input").on("change", function(event) {
-		event.preventDefault();
-
-		var dataId = []
-		$("div#productColumnId ul li input:checked").each(function(index) {
-			name = $(this).attr("id");
-			dataId.push(name);
-		});
-		$("#ajax-box-inner").text(JSON.stringify(dataId));
-		/*
-		$.ajax({
-			url : '/myAjax?method=saveTableConfigList',
-			dataType : "json",
-			contentType : "application/json; charset=utf-8",
-			data : {
-				order : JSON.stringify(dataId),
-				tipo_objecto : 1
-			}
-		});*/
-
-		column_id = $(this).attr("id")
-		if ($(this).prop('checked')) {
-			$('.' + column_id).show();
-		} else {
-			$('.' + column_id).hide();
-		}
-	})
-});
